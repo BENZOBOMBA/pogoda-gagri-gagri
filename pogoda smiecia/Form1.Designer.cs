5cc92e35275c07857380c2c3d72c3d1c1485c330
kubam <kubam@JACOB>
2024-11-29 20:11:09 +0100

    Add project files.

---
+++ pogoda smiecia/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace pogoda_smiecia
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            richTextBox1 = new RichTextBox();
+            button1 = new Button();
+            TemperatureTextBox = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            WilgorTextbox = new TextBox();
+            PogodaTextBox = new TextBox();
+            PredkoscTextBox = new TextBox();
+            KierunekTextBox = new TextBox();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(23, 219);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(291, 219);
+            richTextBox1.TabIndex = 0;
+            richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(342, 21);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 1;
+            button1.Text = "POGODA!!!";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // TemperatureTextBox
+            // 
+            TemperatureTextBox.Location = new Point(175, 18);
+            TemperatureTextBox.Name = "TemperatureTextBox";
+            TemperatureTextBox.Size = new Size(100, 23);
+            TemperatureTextBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(62, 21);
+            label1.Name = "label1";
+            label1.Size = new Size(76, 15);
+            label1.TabIndex = 3;
+            label1.Text = "Temperatura:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(62, 56);
+            label2.Name = "label2";
+            label2.Size = new Size(90, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Wilgor(Wilgoc):";
+            // 
+            // WilgorTextbox
+            // 
+            WilgorTextbox.Location = new Point(175, 56);
+            WilgorTextbox.Name = "WilgorTextbox";
+            WilgorTextbox.Size = new Size(100, 23);
+            WilgorTextbox.TabIndex = 5;
+            // 
+            // PogodaTextBox
+            // 
+            PogodaTextBox.Location = new Point(175, 94);
+            PogodaTextBox.Name = "PogodaTextBox";
+            PogodaTextBox.Size = new Size(139, 23);
+            PogodaTextBox.TabIndex = 6;
+            // 
+            // PredkoscTextBox
+            // 
+            PredkoscTextBox.Location = new Point(175, 134);
+            PredkoscTextBox.Name = "PredkoscTextBox";
+            PredkoscTextBox.Size = new Size(100, 23);
+            PredkoscTextBox.TabIndex = 7;
+            // 
+            // KierunekTextBox
+            // 
+            KierunekTextBox.Location = new Point(175, 178);
+            KierunekTextBox.Name = "KierunekTextBox";
+            KierunekTextBox.Size = new Size(100, 23);
+            KierunekTextBox.TabIndex = 8;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(39, 97);
+            label3.Name = "label3";
+            label3.Size = new Size(113, 15);
+            label3.TabIndex = 9;
+            label3.Text = "Pogoda(kod WMO):";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(62, 134);
+            label4.Name = "label4";
+            label4.Size = new Size(94, 15);
+            label4.TabIndex = 10;
+            label4.Text = "predkosc wiatru:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(58, 181);
+            label5.Name = "label5";
+            label5.Size = new Size(94, 15);
+            label5.TabIndex = 11;
+            label5.Text = "kierunek wiatru: ";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(KierunekTextBox);
+            Controls.Add(PredkoscTextBox);
+            Controls.Add(PogodaTextBox);
+            Controls.Add(WilgorTextbox);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(TemperatureTextBox);
+            Controls.Add(button1);
+            Controls.Add(richTextBox1);
+            DoubleBuffered = true;
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private RichTextBox richTextBox1;
+        private Button button1;
+        private TextBox TemperatureTextBox;
+        private Label label1;
+        private Label label2;
+        private TextBox WilgorTextbox;
+        private TextBox PogodaTextBox;
+        private TextBox PredkoscTextBox;
+        private TextBox KierunekTextBox;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+    }
+}
---
+++ pogoda smiecia/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System.Net.Http.Json;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
+namespace pogoda_smiecia
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Dictionary<int, string> kody = new Dictionary<int, string>
+        {
+            { 0, "Czyste niebo" },
+            { 1, "Głównie czysto" },
+            { 2, "Częściowo pochmurno" },
+            { 3, "Całkowicie pochmurno" },
+            { 45, "Mgła i osadzająca się szadź" },
+            { 48, "Mgła i osadzająca się szadź" },
+            { 51, "Mżawka: Lekka intensywność" },
+            { 53, "Mżawka: Umiarkowana intensywność" },
+            { 55, "Mżawka: Gęsta intensywność" },
+            { 56, "Mżawka lodowa: Lekka intensywność" },
+            { 57, "Mżawka lodowa: Gęsta intensywność" },
+            { 61, "Deszcz: Lekka intensywność" },
+            { 63, "Deszcz: Umiarkowana intensywność" },
+            { 65, "Deszcz: Silna intensywność" },
+            { 66, "Deszcz lodowy: Lekka intensywność" },
+            { 67, "Deszcz lodowy: Silna intensywność" },
+            { 71, "Opady śniegu: Lekka intensywność" },
+            { 73, "Opady śniegu: Umiarkowana intensywność" },
+            { 75, "Opady śniegu: Silna intensywność" },
+            { 77, "Kryształki śniegu" },
+            { 80, "Przelotne opady deszczu: Lekkie" },
+            { 81, "Przelotne opady deszczu: Umiarkowane" },
+            { 82, "Przelotne opady deszczu: Gwałtowne" },
+            { 85, "Przelotne opady śniegu: Lekkie" },
+            { 86, "Przelotne opady śniegu: Silne" },
+            { 95, "Burza: Lekka lub umiarkowana" },
+            { 96, "Burza z lekkim gradem" },
+            { 99, "Burza z silnym gradem" }
+        };
+
+        private string[] dir = ["N", "E", "S", "W"]; 
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            HttpClient client = new HttpClient();
+            string url = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,relative_humidity_2m,weather_code,wind_speed_10m,wind_direction_10m&forecast_days=1";
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            string json = response.Content.ReadAsStringAsync().Result;
+            richTextBox1.Text = json;
+            APIResponse apiResponse = JsonConvert.DeserializeObject<APIResponse>(json);
+            TemperatureTextBox.Text = apiResponse.current.temperature_2m.ToString();
+            TemperatureTextBox.Text += " ";
+            TemperatureTextBox.Text += apiResponse.current_units.temperature_2m;
+            WilgorTextbox.Text = apiResponse.current.relative_humidity_2m.ToString() + " " + apiResponse.current_units.relative_humidity_2m;
+            PogodaTextBox.Text += kody[apiResponse.current.weather_code];
+            PredkoscTextBox.Text = apiResponse.current.wind_speed_10m.ToString() + " " + apiResponse.current_units.wind_speed_10m;
+            KierunekTextBox.Text = dir[(int)Math.Round((double)(apiResponse.current.wind_direction_10m / 90)) % 4]; 
+
+
+        }
+    }
+
+
+
+}
